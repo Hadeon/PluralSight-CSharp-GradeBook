509be085fc736e1028a0d8f0a6a06cbc0c5d9020
Elijah <andersene909@gmail.com>
2016-11-16 13:58:03 -0800

    Letter Grades with Switch Statements
    Switch statements and basic control flow overview

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -13,22 +13,20 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
-            book.NameChanged += new NameChangedDelegate(OneNameChanged);
-            book.Name = "Eli";
+
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(book.Name);
+
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
-
+            WriteResult("Grade", stats.LetterGrade)
         }
-
-        static void OneNameChanged(object sender, NameChangedEventArgs args)
+        static void WriteResult(string description, string result)
         {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+            Console.WriteLine($"{description}: {result}");
         }
 
         static void WriteResult(string description, float result)
